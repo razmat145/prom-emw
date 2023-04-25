86b0c34073803d1bcb5937894debe477502d0ef1
Razvan Matei <monakyo@gmail.com>
2023-04-25 23:00:32 +0100

    more tests

--- test/prom/Recorder.test.ts
+++ test/prom/Recorder.test.ts
@@ -3,24 +3,30 @@ import { describe, it, expect, jest, afterEach } from '@jest/globals';
 import { Registry, Counter, Histogram, collectDefaultMetrics } from 'prom-client';
 
 import { Recorder } from '../../src/lib/prom/Recorder';
+import Extractor from '../../src/lib/prom/Extractor';
 import Session from '../../src/lib/util/Session';
 
 import { defaultLabels } from '../../src/lib/types/Label';
 
 const sessionGetConfigItemSpy = jest.spyOn(Session, 'getConfigItem');
+
+const extractorExtractDefaultLabels = jest.spyOn(Extractor, 'extractDefaultLabels');
+const extractorExtractTimingSeconds = jest.spyOn(Extractor, 'extractTimingSeconds');
+
 jest.mock('prom-client');
 
 describe('Recorder', () => {
+
+    const mockRegister = {
+        registerMetric: jest.fn(),
+        metrics: jest.fn<() => Promise<string>>()
+    };
 
     afterEach(() => {
         jest.clearAllMocks();
     });
 
     describe('constructor', () => {
-
-        const mockRegister = {
-            registerMetric: jest.fn()
-        };
 
         it(`should initialise the default registry`, () => {
             sessionGetConfigItemSpy.mockReturnValue('');
@@ -87,6 +93,57 @@ describe('Recorder', () => {
             expect(collectDefaultMetrics).toHaveBeenCalledWith({ register: mockRegister });
         });
 
+    });
+
+    describe('recordRequestStats', () => {
+        it('should call the extractor and call to record the extracted values', () => {
+            const mockReq = 'mockReq101';
+            const mockRes = 'mockRes101';
+            const mockTiming: [number, number] = [101, 101];
+
+            const mockDefaultLabels = 'defaultLabels101';
+            const mockTimingSeconds = 'timingSeconds101';
+
+            const requestCounterSpy = jest.fn();
+            const timingHistogramSpy = jest.fn();
+
+            const sutRecorder = new Recorder();
+
+            sutRecorder['requestCounter'] = <any>{ inc: requestCounterSpy };
+            sutRecorder['timingHistogram'] = <any>{ observe: timingHistogramSpy };
+
+            extractorExtractDefaultLabels.mockReturnValue(<any>mockDefaultLabels);
+            extractorExtractTimingSeconds.mockReturnValue(<any>mockTimingSeconds);
+
+            sutRecorder.recordRequestStats(<any>mockReq, <any>mockRes, mockTiming);
+
+            expect(extractorExtractDefaultLabels).toHaveBeenCalled();
+            expect(extractorExtractDefaultLabels).toHaveBeenCalledWith(mockReq, mockRes);
+
+            expect(extractorExtractTimingSeconds).toHaveBeenCalled();
+            expect(extractorExtractTimingSeconds).toHaveBeenCalledWith(mockTiming);
+
+            expect(requestCounterSpy).toHaveBeenCalled();
+            expect(requestCounterSpy).toHaveBeenCalledWith(mockDefaultLabels);
+
+            expect(timingHistogramSpy).toHaveBeenCalled();
+            expect(timingHistogramSpy).toHaveBeenCalledWith(mockDefaultLabels, mockTimingSeconds);
+        });
+    });
+
+    describe('getMetrics', () => {
+        it('should call to return the registry\'s metrics', async () => {
+            (<jest.Mock><unknown>Registry).mockReturnValue(mockRegister);
+
+            const mockReturn = 'mockReturn101';
+            mockRegister.metrics.mockResolvedValue(mockReturn);
+
+            const sutRecorder = new Recorder();
+            const sutResult = await sutRecorder.getMetrics();
+
+            expect(mockRegister.metrics).toHaveBeenCalled();
+            expect(sutResult).toEqual(mockReturn);
+        });
     });
 
 });
